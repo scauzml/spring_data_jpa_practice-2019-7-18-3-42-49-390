0acb6dfc47a1bec44d1b9c85b74c749742ae4670
scauzml <13828607279@163.com>
2019-07-18 18:41:56 +0800

    age大于18

--- src/main/resources/db/migration/V3__alter_column_property.sql
+++ src/main/resources/db/migration/V3__alter_column_property.sql
@@ -1,4 +1,5 @@
 ALTER TABLE prosecutor MODIFY COLUMN age int;
+ALTER TABLE prosecutor add CONSTRAINT age check(age>18);
 ALTER TABLE prosecutor add CONSTRAINT sex check(sex= '男' or sex= '女' );
 ALTER TABLE prosecutor add CONSTRAINT experience check(experience >=0);
 
